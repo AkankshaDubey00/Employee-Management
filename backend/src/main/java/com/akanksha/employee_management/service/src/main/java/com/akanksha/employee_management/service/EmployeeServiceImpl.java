aca67536696b076bdbca834eda401eacf8f8df04
Akanksha Dubey <dubeyakankshadubey95@gmail.com>
2026-09-04 09:01:11 +0530

    Create EmployeeServiceImpl.java

---
+++ backend/src/main/java/com/akanksha/employee_management/service/src/main/java/com/akanksha/employee_management/service/EmployeeServiceImpl.java
@@ -0,0 +1,53 @@
+package com.akanksha.employee_management.service;
+
+import com.akanksha.employee_management.entity.Employee;
+import com.akanksha.employee_management.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+
+        Employee existingEmployee = getEmployeeById(id);
+
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+        existingEmployee.setDepartment(employee.getDepartment());
+        existingEmployee.setSalary(employee.getSalary());
+
+        return employeeRepository.save(existingEmployee);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        Employee employee = getEmployeeById(id);
+        employeeRepository.delete(employee);
+    }
+}
