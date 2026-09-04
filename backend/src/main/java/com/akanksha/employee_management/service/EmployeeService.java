df7cddb1045c3d2378edc672c53ad45beb10a6d4
Akanksha Dubey <dubeyakankshadubey95@gmail.com>
2026-09-04 09:00:24 +0530

    Create EmployeeService.java

---
+++ backend/src/main/java/com/akanksha/employee_management/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.akanksha.employee_management.service;
+
+import com.akanksha.employee_management.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+    void deleteEmployee(Long id);
+}
